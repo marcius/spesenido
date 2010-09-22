0c303c284b59cf28d17bc7bad42aac52a1b64cc8
marcius <marcius@d092d19a-2706-4ded-9a81-17733914776e>
2010-09-22 21:54:11 +0000

    git-svn-id: https://spesenido.svn.sourceforge.net/svnroot/spesenido/trunk@18 d092d19a-2706-4ded-9a81-17733914776e

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
- $yii=dirname(__FILE__).'/../yii-1.1.4.r2429/framework/yii.php';
+ $yii=dirname(__FILE__).'/../../yii-1.1.4.r2429/framework/yii.php';
 //$yii=dirname(__FILE__).'/../yii-1.1.2.r2086/framework/yii.php';
 //$config=dirname(__FILE__).'/protected/config/main.php';
 
